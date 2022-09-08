c44177849c38db7ff638ef8bf1c3b44e4bec7dd6
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-09-08 16:04:46 +0300

    dizayn and new features

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>
-      About
-    </div>
-  )
+    <section className="aboutContent">
+      <div className="about">
+        <div className="text">
+          <h2>About</h2>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia
+          saepe similique incidunt veritatis quos sunt, perferendis, expedita
+          consectetur dolore perspiciatis accusamus, natus alias nulla
+          voluptates reprehenderit quidem debitis? Fuga minima amet in esse
+          itaque sapiente sit ipsa aspernatur placeat sunt.
+        </div>
+        <div className="userImg">
+          <img src="https://avatars.githubusercontent.com/u/81258778?v=4" alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
-export default About
+export default About;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -23,14 +23,64 @@ export default class Country extends Component {
         let data = this.state.country;
         countrs.innerHTML = "";
         country.innerHTML = `
-         <div class="card" style="width: 18rem;">
-                 <img src="${data.flags.png}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                         <h5 class="card-title">${data.name.official}</h5>
-                         <p class="card-text">${data.name.common}</p>
-                         ${data.name.common === 'Turkey'  ? 'Ne Mutlu Turkum Diyene , Ulu Önder Mustafa Kemal Atatürk'  : ''}
-                    </div>
-        </div>`;
+        <div>
+        <div class=card">
+          <img
+            src="${data.flags.png}"
+            class="card-img-top"
+            alt=".asdas"
+          />
+          <div class="card-head">
+            <h5 class="card-title">Offical ${data.name.official}</h5>
+            <p class="card-text">${(data.name.common, data.capital[0])}</p>
+          </div>
+          <div class="card-body">
+            <div class="translationsContent">
+              <div class="mapAndTranslations">
+                <div>
+                  <h5>Translations</h5>
+                </div>
+                /
+                <div class="mapAndLanguage">
+                  ${data.region} ,${data.subregion}
+                </div>
+              </div>
+              <div class="translations">
+                <div class="translation">${data.translations.ara.common}</div>
+                <div class="translation">${data.translations.bre.common}</div>
+                <div class="translation">${data.translations.ces.common}</div>
+                <div class="translation">${data.translations.cym.common}</div>
+                <div class="translation">${data.translations.deu.common}</div>
+                <div class="translation">${data.translations.fra.common}</div>
+                <div class="translation">${data.translations.kor.common}</div>
+                <div class="translation">${data.translations.jpn.common}</div>
+                <div class="translation">${data.translations.hrv.common}</div>
+                <div class="translation">${data.translations.hun.common}</div>
+                <div class="translation">${data.translations.ita.common}</div>
+                <div class="translation">${data.translations.per.common}</div>
+                <div class="translation">${data.translations.pol.common}</div>
+                <div class="translation">${data.translations.por.common}</div>
+                <div class="translation">${data.translations.rus.common}</div>
+                <div class="translation">${data.translations.zho.common}</div>
+              </div>
+            </div>
+            <div class="borders">
+            <div class="bordersAndMaps">
+              <div>
+              <h5>Borders</h5>
+              </div>
+              <div>
+                <a href="${data.maps.googleMaps}">Maps</a>
+              </div>
+            </div>
+             <p>${data.borders.slice(0,10)}</p>
+            </div>
+            <div class="pop">
+             population : ${data.population}
+            </div>
+          </div>
+        </div>
+      </div>`;
       } else {
         return <div>yok</div>;
       }
@@ -38,10 +88,11 @@ export default class Country extends Component {
   };
   render() {
     return (
-      <div>
+      <div className="country">
         <div id="countrs">Loadingg.....</div>
         <div id="country">{this.textArea()}</div>
       </div>
     );
   }
 }
+
